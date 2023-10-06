99efc67cce5e402faf1baabb576cfacdcfb20b86
somuyakhandelwal <141111077+somuyakhandelwal@users.noreply.github.com>
2023-10-06 15:07:56 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,37 +1,74 @@
+// Initialize an empty array to store activities
 var listOfActivities = [];
+
+// Get references to HTML elements
 var input = document.getElementById("input");
 var todolist = document.getElementById("todolist");
 
-document.getElementById("button").onclick = click;
-function click() {
-  listOfActivities.push(input.value);
-  console.log(listOfActivities);
-  input.value = "";
-  showList();
+// Add a click event listener to a button
+document.getElementById("button").onclick = handleButtonClick;
+
+// Event handler for button click
+function handleButtonClick() {
+  // Get the input value and add it to the list of activities
+  var newActivity = input.value.trim();
+  if (newActivity !== "") {
+    listOfActivities.push(newActivity);
+    input.value = "";
+    showList();
+  }
 }
 
-
+// Display the list of activities
 function showList() {
-  todolist.innerHTML = " ";
-  listOfActivities.forEach(function (n, i) {
-    todolist.innerHTML +=
-      "<li>" +
-      n +
-      "<a onclick='editItem(" +
-      i +
-      ")'>Edit</a>" +
-      "<a onclick='deleteItem(" +
-      i +
-      ")'>&times | </a></li>";
+  // Clear the previous list
+  todolist.innerHTML = "";
+
+  // Loop through the list of activities and create list items
+  listOfActivities.forEach(function (activity, index) {
+    var listItem = document.createElement("li");
+
+    // Create a text node for the activity
+    var textNode = document.createTextNode(activity);
+
+    // Create an edit button
+    var editButton = document.createElement("a");
+    editButton.textContent = "Edit";
+    editButton.onclick = function () {
+      editItem(index);
+    };
+
+    // Create a delete button
+    var deleteButton = document.createElement("a");
+    deleteButton.textContent = "Delete";
+    deleteButton.onclick = function () {
+      deleteItem(index);
+    };
+
+    // Append elements to the list item
+    listItem.appendChild(textNode);
+    listItem.appendChild(editButton);
+    listItem.appendChild(deleteButton);
+
+    // Append the list item to the to-do list
+    todolist.appendChild(listItem);
   });
 }
 
-function deleteItem(i) {
-  listOfActivities.splice(i, 1);
+// Delete an item from the list
+function deleteItem(index) {
+  listOfActivities.splice(index, 1);
   showList();
 }
-function editItem(i) {
-  var newValue = prompt("Please insert your new value");
-  listOfActivities.splice(i, 1, newValue);
- showList();
+
+// Edit an item in the list
+function editItem(index) {
+  var newValue = prompt("Please enter a new value");
+  if (newValue !== null) {
+    listOfActivities[index] = newValue;
+    showList();
+  }
 }
+
+// Initial display of the empty list
+showList();
